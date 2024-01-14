8842349e4dc5c658e8792ae066a15a1318f1846a
noriapi <70106808+noriapi@users.noreply.github.com>
2024-01-15 04:35:45 +0900

    add WysiwygConfigInput component

---
+++ src/components/WysiwygConfigInput.tsx
@@ -0,0 +1,13 @@
+import { Component } from "solid-js";
+
+import FakeChat from "./FakeChat";
+
+const WysiwygConfigInput: Component<{}> = (props) => {
+  return (
+    <div>
+      <FakeChat />
+    </div>
+  );
+};
+
+export default WysiwygConfigInput;
--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { Component } from "solid-js";
 
 import ConfigForm from "~/components/ConfigForm";
+import WysiwygConfigInput from "~/components/WysiwygConfigInput";
 import { CONFIG_DEFAULT } from "~/lib/config";
 
 const App: Component = () => {
-  return <ConfigForm defaultValue={CONFIG_DEFAULT} />;
+  // return <ConfigForm defaultValue={CONFIG_DEFAULT} />;
+  return <WysiwygConfigInput />;
 };
 
 export default App;
