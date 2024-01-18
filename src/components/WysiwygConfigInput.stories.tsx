028db39a809d4004f2b96e4630d696f870751949
noriapi <70106808+noriapi@users.noreply.github.com>
2024-01-19 04:20:21 +0900

    add WysiwygConfigInput story

---
+++ src/components/WysiwygConfigInput.stories.tsx
@@ -0,0 +1,53 @@
+import type { ComponentProps } from "solid-js";
+import { createStore } from "solid-js/store";
+import type { Meta, StoryObj } from "storybook-solidjs";
+
+import { type Config } from "~/lib/config";
+
+import WysiwygConfigInput from "./WysiwygConfigInput";
+
+type Props = ComponentProps<typeof WysiwygConfigInput> & {
+  initialValue: Config;
+};
+
+const meta = {
+  component: WysiwygConfigInput,
+  render: (props) => {
+    const [config, setConfig] = createStore<Config>(props.initialValue);
+
+    return <WysiwygConfigInput value={config} setValue={setConfig} />;
+  },
+} satisfies Meta<Props>;
+
+export default meta;
+type Story = StoryObj<Props>;
+
+export const Default = {
+  args: {
+    initialValue: {
+      message: {
+        owner: "show",
+        moderator: "show",
+        others: "show",
+      },
+      name: {
+        owner: "show",
+        moderator: "show",
+        others: "show",
+      },
+      icon: {
+        owner: "show",
+        moderator: "show",
+        others: "show",
+      },
+      badge: {
+        moderator: "show",
+      },
+      superchat: "show",
+      sticker: "show",
+      superchatBar: "show",
+      memberChat: "show",
+      engagement: "show",
+    },
+  },
+} satisfies Story;
